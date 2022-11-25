ab276d6d6ded4eb1f95e1854dc77f2e09379b242
Mikhail Varushichev <mikeloangel@gmail.com>
2022-11-25 11:26:02 +0100

    release: adds frontend release

---
+++ public/precache-manifest.e08cb0866ed343ec128732a2de9a4661.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bdf7c2dca07aebbe3e50c7e6b7f416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63717480db3517681e66",
+    "url": "/static/css/main.6eab0c30.chunk.css"
+  },
+  {
+    "revision": "ab4195c7022ca650903b",
+    "url": "/static/js/2.ccbee4e9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ccbee4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63717480db3517681e66",
+    "url": "/static/js/main.652037d6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.652037d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd0d6f97b6bbffde950",
+    "url": "/static/js/runtime-main.f7f8c00c.js"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  }
+]);
